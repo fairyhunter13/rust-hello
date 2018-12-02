1e1653b9a47d8455454ef33a51e19b5b8f024490
fairyhunter13 <fairyhunter13@gmail.com>
2018-12-02 23:59:35 +0700

    Progress: - Learning iterators and closures :construction:

---
+++ iterators_and_closures/src/lib.rs
@@ -0,0 +1,100 @@
+pub struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    pub fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation: calculation,
+            value: None,
+        }
+    }
+
+    pub fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoe_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes
+        .into_iter()
+        .filter(|shoe| shoe.size == shoe_size)
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn filter_by_size() {
+        let shoes = vec![
+            Shoe {
+                size: 10,
+                // style: "sneaker".to_owned(),
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 13,
+                style: "sandal".to_owned(),
+            },
+            Shoe {
+                size: 10,
+                style: "boot".to_owned(),
+            },
+        ];
+    }
+
+    #[test]
+    fn call_with_different_values() {
+        let mut c = Cacher::new(|a| a);
+
+        let v1 = c.value(1);
+        let v2 = c.value(2);
+
+        assert_eq!(v2, 2);
+    }
+
+    #[test]
+    fn iterator_demonstration() {
+        let v1 = vec![1, 2, 3];
+
+        let mut v1_iter = v1.iter();
+
+        assert_eq!(v1_iter.next(), Some(&1));
+        assert_eq!(v1_iter.next(), Some(&2));
+        assert_eq!(v1_iter.next(), Some(&3));
+        assert_eq!(v1_iter.next(), None);
+    }
+
+    #[test]
+    fn iterator_sum() {
+        let v1 = vec![1, 2, 3];
+
+        let v1_iter = v1.iter();
+
+        let total: i32 = v1_iter.sum();
+
+        assert_eq!(total, 6);
+    }
+}
---
+++ iterators_and_closures/src/main.rs
@@ -0,0 +1,113 @@
+extern crate iterators_and_closures;
+
+use iterators_and_closures::Cacher;
+use std::{thread, time::Duration};
+
+fn main() {
+    // println!("Hello, world!");
+    // let simulated_user_specified_value = 10;
+    // let simulated_random_number = 2;
+
+    // generate_workout(simulated_user_specified_value, simulated_random_number);
+
+    // let x = 4;
+    // let equal_to_x = |z| z == x;
+    // let y = 4;
+    // assert!(equal_to_x(y));
+
+    let x = vec![1, 2, 3];
+
+    let equal_to_x = move |z| z == x;
+
+    // println!("can't use x here: {:?}", x);
+
+    let y = vec![1, 2, 3];
+    assert!(equal_to_x(y));
+
+    let v1 = vec![1, 2, 3];
+    let v1_iter = v1.iter();
+
+    for val in v1_iter {
+        println!("Item: {}", val);
+    }
+
+    let v1: Vec<u32> = vec![1, 2, 3];
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+
+    assert_eq!(v2, vec![2, 3, 4]);
+}
+
+// fn generate_workout(intensity: u32, random_number: u32) {
+//     if intensity < 25 {
+//         println!(
+//             "Today do {} pushups!",
+//             simulated_expensive_calculation(intensity)
+//         );
+//         println!(
+//             "Today do {} situps!",
+//             simulated_expensive_calculation(intensity)
+//         );
+//     } else {
+//         if random_number == 3 {
+//             println!("Take a break today! Remember to stay hidrated!");
+//         } else {
+//             println!(
+//                 "Today, run for {} minutes!",
+//                 simulated_expensive_calculation(intensity)
+//             );
+//         }
+//     }
+// }
+
+// fn generate_workout(intensity: u32, random_number: u32) {
+//     let expensive_results = simulated_expensive_calculation(intensity);
+//     if intensity < 25 {
+//         println!("Today do {} pushups!", expensive_results);
+//         println!("Today do {} situps!", expensive_results);
+//     } else {
+//         if random_number == 3 {
+//             println!("Take a break today! Remember to stay hidrated!");
+//         } else {
+//             println!("Today, run for {} minutes!", expensive_results);
+//         }
+//     }
+// }
+
+// fn generate_workout(intensity: u32, random_number: u32) {
+//     let expensive_closure = |num: u32| -> u32 {
+//         println!("calculating slowly!");
+//         thread::sleep(Duration::from_secs(2));
+//         intensity
+//     };
+//     if intensity < 25 {
+//         println!("Today do {} pushups!", expensive_closure(intensity));
+//         println!("Today do {} situps!", expensive_closure(intensity));
+//     } else {
+//         if random_number == 3 {
+//             println!("Take a break today! Remember to stay hidrated!");
+//         } else {
+//             println!("Today, run for {} minutes!", expensive_closure(intensity));
+//         }
+//     }
+// }
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive_results = Cacher::new(|num: u32| -> u32 {
+        println!("calculating slowly!");
+        thread::sleep(Duration::from_secs(2));
+        intensity
+    });
+    if intensity < 25 {
+        println!("Today do {} pushups!", expensive_results.value(intensity));
+        println!("Today do {} situps!", expensive_results.value(intensity));
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hidrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                expensive_results.value(intensity)
+            );
+        }
+    }
+}

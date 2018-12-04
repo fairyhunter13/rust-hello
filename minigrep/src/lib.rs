fb3577935228eaf5546449b89a6aeaa067cae4fc
fairyhunter13 <fairyhunter13@gmail.com>
2018-12-04 12:41:29 +0700

    Progress: - Learning iterators_and_clsoures :sparkles:

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,4 +1,8 @@
-use std::{env, error::Error, fs};
+use std::{
+    env::{self, Args},
+    error::Error,
+    fs,
+};
 
 pub struct Config {
     pub query: String,
@@ -7,16 +11,38 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments!");
-        }
+    // pub fn new(args: &[String]) -> Result<Config, &str> {
+    //     if args.len() < 3 {
+    //         return Err("Not enough arguments!");
+    //     }
+
+    //     let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+    //     Ok(Config {
+    //         query: args[1].to_owned(),
+    //         filename: args[2].to_owned(),
+    //         case_sensitive: case_sensitive,
+    //     })
+    // }
+
+    pub fn new(mut args: Args) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(val) => val,
+            None => return Err("Didn't get the query string!"),
+        };
+
+        let filename = match args.next() {
+            Some(val) => val,
+            None => return Err("Didn't get a filename!"),
+        };
 
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
         Ok(Config {
-            query: args[1].to_owned(),
-            filename: args[2].to_owned(),
+            query: query,
+            filename: filename,
             case_sensitive: case_sensitive,
         })
     }
@@ -41,13 +67,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results: Vec<&'a str> = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line)
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {

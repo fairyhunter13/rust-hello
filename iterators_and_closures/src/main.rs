4951a58f94bdb3c8fa45085de34d5e9c314de606
fairyhunter13 <fairyhunter13@gmail.com>
2018-12-04 13:09:37 +0700

    Progress: - Learning iterators_and_closures :sparkles:

--- iterators_and_closures/src/main.rs
+++ iterators_and_closures/src/main.rs
@@ -39,6 +39,21 @@ fn main() {
     for num in Counter::new().skip(1) {
         println!("Test skip: {}", num);
     }
+
+    // let buffer: &mut [i32];
+    // let coefficients: [i64; 12];
+    // let qlp_shift: i16;
+
+    // for i in 12..buffer.len() {
+    //     let prediction = coefficients
+    //         .iter()
+    //         .zip(&buffer[i - 12..i])
+    //         .map(|(&c, &s)| c * s as i64)
+    //         .sum::<i64>()
+    //         >> qlp_shift;
+    //     let delta = buffer[i];
+    //     buffer[i] = prediction as i32 + delta;
+    // }
 }
 
 // fn generate_workout(intensity: u32, random_number: u32) {

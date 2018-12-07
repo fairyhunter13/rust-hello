2dd539aabf2164632c66897a56963b796ec0afd2
fairyhunter13 <fairyhunter13@gmail.com>
2018-12-08 06:36:30 +0700

    Progress: - Learnign Ref :construction:

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,8 +1,13 @@
-use std::ops::Deref;
+use std::{ops::Deref, rc::Rc};
 use List::{Cons, Nil};
 
+// enum List {
+//     Cons(i32, Box<List>),
+//     Nil,
+// }
+
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
@@ -38,7 +43,7 @@ fn main() {
     println!("b = {}", b);
     println!("k = {}", k);
 
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    // let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 
     let x = 5;
     let y = &x;
@@ -80,6 +85,22 @@ fn main() {
     drop(c);
 
     println!("CustomSmartPointer dropped before the end of main.");
+
+    // This won't work because c and b have multiple ownership to a.
+    // let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
+    // let b = Cons(3, Box::new(a));
+    // let c = Cons(4, Box::new(a));
+
+    // We do this instead.
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Rc::new(Cons(3, Rc::clone(&a)));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Rc::new(Cons(4, Rc::clone(&a)));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c is out of scope = {}", Rc::strong_count(&a));
 }
 
 fn hello(name: &str) {

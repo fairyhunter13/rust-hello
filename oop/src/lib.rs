557dd6c65cfa2d88008cb67100d01770f7d6412e
fairyhunter13 <fairyhunter13@gmail.com>
2018-12-09 19:15:40 +0700

    - Learning using trait objects :construction:

---
+++ oop/src/lib.rs
@@ -0,0 +1,67 @@
+pub struct AveragedCollection {
+    list: Vec<i32>,
+    average: f64,
+}
+
+impl AveragedCollection {
+    pub fn add(&mut self, value: i32) {
+        self.list.push(value);
+        self.update_average();
+    }
+
+    pub fn remove(&mut self) -> Option<i32> {
+        let result = self.list.pop();
+
+        match result {
+            Some(value) => {
+                self.update_average();
+                Some(value)
+            }
+            None => None,
+        }
+    }
+
+    pub fn average(&self) -> f64 {
+        self.average
+    }
+
+    fn update_average(&mut self) {
+        let total: i32 = self.list.iter().sum();
+        self.average = total as f64 / self.list.len() as f64;
+    }
+}
+
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+// This code below only works in compile time (monomorphization).
+// This code is likely failed if at runtime the new type comes (new types that implements Draw Trait).
+// Using generic only limit to the homogenous collection only.
+
+// pub struct Screen<T: Draw> {
+//     pub components: Vec<T>,
+// }
+
+// impl<T> Screen<T>
+// where
+//     T: Draw,
+// {
+//     pub fn run(&self) {
+//         for component in self.components.iter() {
+//             component.draw();
+//         }
+//     }
+// }

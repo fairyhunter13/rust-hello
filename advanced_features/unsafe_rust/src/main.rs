c4173fa8b96b11fccaec3d40dc291a98bbb5679e
fairyhunter13 <fairyhunter13@gmail.com>
2018-12-22 12:07:33 +0700

    - Learning offset from raw pointer :sparkles:

--- advanced_features/unsafe_rust/src/main.rs
+++ advanced_features/unsafe_rust/src/main.rs
@@ -31,7 +31,9 @@ fn main() {
 
     let r = &mut v[..];
 
-    let (a, b) = r.split_at_mut(3);
+    let (a, b) = split_at_mut(r, 3);
+
+    // let (a, b) = r.split_at_mut(3);
 
     assert_eq!(a, &mut [1, 2, 3]);
     assert_eq!(b, &mut [4, 5, 6]);
@@ -84,6 +86,8 @@ fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
     assert!(mid <= len);
 
     unsafe {
+        println!("The first element: {:?}", *ptr);
+        println!("The second element: {:?}", *ptr.offset(mid as isize));
         (
             slice::from_raw_parts_mut(ptr, mid),
             slice::from_raw_parts_mut(ptr.offset(mid as isize), len - mid),

12dfd30b579d732644c7b80050a6bd90a58fe899
fairyhunter13 <fairyhunter13@gmail.com>
2018-12-24 22:52:06 +0700

    - Removing unused imports :fire:

--- hello/src/bin/main.rs
+++ hello/src/bin/main.rs
@@ -1,7 +1,7 @@
 extern crate hello;
 
 use hello::ThreadPool;
-use std::{net::TcpListener, thread};
+use std::{net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();

3f0e883edc0090a961343a315bbea3bac7a2de53
fairyhunter13 <fairyhunter13@gmail.com>
2018-12-24 22:53:36 +0700

    - Fix typo :pencil2:

--- hello/src/bin/main.rs
+++ hello/src/bin/main.rs
@@ -5,7 +5,7 @@ use std::{net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = Threadpool::new(4);
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
